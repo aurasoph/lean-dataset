65f95a5adafdf429e074917d7c13ed7b65332fef
xvade <simon@vantuyl.com>
2025-06-02 14:31:28 -0700

    Renamed one_at_a_time.py to lean_ai_tools.py, added laptop.py and amath.py for delegating lean compilation to my laptop

---
+++ Model Testing/laptop.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request
+import uvicorn
+from lean_interact import LeanREPLConfig, TempRequireProject, LeanRequire, LeanServer, FileCommand, Command
+import lean_ai_tools
+
+LEAN_CONFIG = LeanREPLConfig(
+    lean_version="v4.19.0",
+    project=TempRequireProject([
+        LeanRequire(
+            name="mathlib",
+            git="https://github.com/leanprover-community/mathlib4.git",
+            rev="v4.19.0"
+        )
+    ])
+)
+
+lean_server = LeanServer(LEAN_CONFIG)
+
+app = FastAPI()
+
+@app.post("/ping")
+async def ping():
+    return {"message" : "pong"}
+
+@app.post("/lean-response")
+async def compile_request(request: Request):
+    res = await request.json()
+    data = res["lean"]
+    result = lean_server.run(Command(cmd=data))
+    return {"message" : lean_ai_tools.generate_reply(result.messages)}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
